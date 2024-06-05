c0096c9e012bff6492b38b5c56e74f7af8becc7c
pablohg2023 <pablohg2014@gmail.com>
2024-06-05 21:09:21 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
